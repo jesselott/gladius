ef57ae09f821324179f554c198263ff91a634662
jesselott <jesse.e.lott@outlook.com>
2016-12-08 15:47:14 -0500

    v 1.8
    **added rudimentary level up function

---
+++ Gladius_1-8/Gladiator/Fighter.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladius
+{
+    //this class defines the hero by holding his fields and properties.  It also defines how the fighter 
+    //(and his counterpart) do their fighting.  Stay tuned for more derivations for this fighter class,
+    //possibly a menu option to choose a class with special skills or spells, etc.
+
+    public class Fighter
+    {
+        //Fighter class fields
+        #region Fighter fields
+        private string fName/*CharactersName*/;
+        private int fHP/*CurrentHitPoints*/, fMHP/*MaxHitPoints*/, fXP/*ExperiencePoints*/, fLvl = 1/*Level*/, 
+            fStr/*Strength*/, fAgl/*Agility*/, fInt/*Intelligence*/, fLck/*Luck*/, fChr/*Charisma*/,
+            fCon/*Constitution*/, fSpr/*Spirit*/, fFry/*Fury*/, fNot/*Noteriety*/, fFam/*Fame*/,
+            fMny/*Money*/,fMnyErn/*MoneyEarned*/, fKills/*Kills*/;
+        private object fWeapon, fArmor;
+        #endregion
+
+        //Fighter class properties
+        #region Fighter Class Props
+
+        public string Name
+        { get { return fName; } set { fName = value; } }
+
+        public object Weapon
+        { get { return fWeapon;} set {fWeapon = value;} }
+
+        public object Armor
+        { get { return fArmor; } set { fArmor = value; } }
+
+        public int Strength
+        { get { return fStr; } set { fStr = value; } }
+
+        public int Agility
+        { get { return fAgl; } set { fAgl = value; } }
+
+        public int Intelligence
+        { get { return fInt; } set { fInt = value; } }
+
+        public int Luck
+        { get { return fLck; } set { fLck = value; } }
+
+        public int Charisma
+        { get { return fChr; } set { fChr = value; } }
+
+        public int Constitution
+        { get { return fCon; } set { fCon = value; } }
+
+        public int Spirit
+        { get { return fSpr; } set { fSpr = value; } }
+
+        public int Fury
+        { get { return fFry; } set { fFry = value; } }
+
+        public int Notoriety
+        { get { return fNot; } set { fNot = value; } }
+
+        public int Fame
+        { get { return fFam; } set { fFam = value; } }
+
+        public int Money
+        { get { return fMny; } set { fMny = value; } }
+
+        public int MoneyEarned
+        { get { return fMnyErn; } set { fMnyErn = value; } }
+
+        public int CurrentHitPoints
+        { get { return fHP; } set { fHP = value; } }
+
+        public int MaxHitPoints
+        { get { return fMHP; } set { fMHP = value; } }
+
+        public int ExperiencePoints
+        { get { return fXP; } set { fXP = value; } }
+
+        public int Level
+        { get { return fLvl; } set { fLvl = value; } }
+
+        public int Kills
+        { get { return fKills; } set { fKills = value; } }
+
+
+        #endregion
+
+        //Fighter base constructor
+        #region Fighter Ctor, uses a dictionary built in the Roller class to add the core stats to fields and props
+
+        public Fighter()
+        {
+            Name = "Glad Jr.";
+
+        }
+        
+        public Fighter (Dictionary<string, int>RollCharStats)
+        {
+           int value;
+            Name = "Glad Jr.";
+            if (RollCharStats.TryGetValue("Strength", out value))
+            { Strength = value; }
+            if (RollCharStats.TryGetValue("Agility", out value))
+            { Agility = value; }
+            if (RollCharStats.TryGetValue("Intelligence", out value))
+            { Intelligence = value; }
+            if (RollCharStats.TryGetValue("Luck", out value))
+            { Luck = value; }
+            if (RollCharStats.TryGetValue("Charisma", out value))
+            { Charisma = value; }
+            if (RollCharStats.TryGetValue("Constitution", out value))
+            { Constitution = value; }
+            Spirit = 0;
+            Fury = 0;
+            Notoriety = 0;
+            Fame = 0;
+            Money = 500;
+            if (RollCharStats.TryGetValue("CurrentHitPoints", out value))
+            { CurrentHitPoints = value; }
+            if (RollCharStats.TryGetValue("MaxHitPoints", out value))
+            { MaxHitPoints = value; }
+            ExperiencePoints = 0;
+            Level = 1;                          
+        }
+        #endregion
+
+        //Fighter methods
+        #region Fighter Methods
+        public int AttackEnemy()
+        {
+            int toHit = Roller.rollXSidedDice(20,1,1) + (Agility + (Strength / 2) + (Intelligence / 2) + (Luck / 2));
+           
+            return toHit;
+        }
+       
+        public int Avoid()
+        {
+            int avoid = Roller.rollXSidedDice(20,1,1) + (Agility + (Constitution / 2) + (Intelligence / 2) + (Luck / 2));
+
+            return avoid;
+            
+        }
+
+        public int CalculateDamage()
+        { 
+           int inflict = ((Strength / 3) + (Roller.rollXSidedDice(12,1,1)));
+
+           return inflict;
+        }
+
+        public int ReceiveDamage(int damage)
+        {
+            CurrentHitPoints -= damage;
+            return damage;
+        }
+        public static int CalculateBaseAtkBonus(int Level)//not implemented yet
+        {//calculates attack bonus to modify attack roll
+            int baseAtkBonus = Level;
+            int additionalAtkBonus = -5 + Level;
+            int thirdAtkBonus = -11 + Level;  
+            int sumAtkBonus = 0;
+            if (Level < 6)
+                sumAtkBonus = baseAtkBonus;
+            else if (Level < 12)
+                sumAtkBonus = baseAtkBonus + additionalAtkBonus;
+            else
+                sumAtkBonus = baseAtkBonus + additionalAtkBonus + thirdAtkBonus;
+
+            return sumAtkBonus;
+        }
+
+        public static int CalculateStrengthBonus(int Strength)//not implemented yet
+        {//calculates the effect of strength on melee attack roll / damage)
+            int strengthBonus = 0;
+            if (Strength > 21)
+                strengthBonus = 6;
+            else if (Strength > 19)
+                strengthBonus = 5;
+            else if (Strength > 17)
+                strengthBonus = 4;
+            else if (Strength > 15)
+                strengthBonus = 3;
+            else if (Strength > 13)
+                strengthBonus = 2;
+            else if (Strength > 11)
+                strengthBonus = 1;
+            else if (Strength < 10)
+                strengthBonus = -1;
+            else if (Strength < 8)
+                strengthBonus = -2;
+            else if (Strength < 6)
+                strengthBonus = -3;
+            else
+                strengthBonus = 0;
+
+            return strengthBonus;
+        }
+        public static int CalculateAgilityBonus(int Agility)
+        {//calculates the effect of agility on ranged attack roll / armor class)
+            int agilityBonus = 0;
+            if (Agility > 21)
+                agilityBonus = 6;
+            else if (Agility > 19)
+                agilityBonus = 5;
+            else if (Agility > 17)
+                agilityBonus = 4;
+            else if (Agility > 15)
+                agilityBonus = 3;
+            else if (Agility > 13)
+                agilityBonus = 2;
+            else if (Agility > 11)
+                agilityBonus = 1;
+            else if (Agility < 10)
+                agilityBonus = -1;
+            else if (Agility < 8)
+                agilityBonus = -2;
+            else if (Agility < 6)
+                agilityBonus = -3;
+            else
+                agilityBonus = 0;
+
+            return agilityBonus;
+        }
+        public void Die()
+        {
+            Console.WriteLine("{0} has died and has gone to Valhalla with the others who \n have died a glorious death in the arena.", Name);
+            Console.WriteLine("Do you wish to start a new game?");
+            string yesno = Console.ReadLine();
+            yesno = yesno.ToLower();
+            switch (yesno)
+            {
+                case "y":
+                    {
+                        //enter game stats here
+                        MainMenu.CharCreator();
+                        break;
+                    }
+                case "n":
+                    {
+                        //enter stats here (kills, money, fame, etc.)
+                        Console.WriteLine("Come back to the arena soon");
+                        MainMenu.QuitGame();
+                        break;
+                        
+                    }
+                default:
+                    MainMenu.QuitGame();
+                    break;
+                    
+            }
+            
+        }
+        #endregion
+
+    }//end Fighter Class
+
+
+}//end namespace
---
+++ Gladius_1-8/Gladiator/Roller.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladius
+{   
+    //this class will return values for "dice" used by the Program for various
+    //uses, to include character creation, combat, etc.
+
+    public static class Roller
+        {
+        
+        //creates a static Random object that can be called throughout the entire namespace without red squigglies.
+        private static Random rnd = new Random();
+        //instance variable that is the return variable for rollXSidedDice() 
+        public static int result;
+        // get the corresponding die type from the method call 
+        // and set diceType to the value of the argument 
+        // passed
+        public static int rollXSidedDice(int diceType,int minRoll, int numberOfDice)
+         {
+
+            result = 0;
+
+            for (int i = 0; i < numberOfDice ; i++)
+            {
+                result += rnd.Next(minRoll, diceType + 1);
+            }
+
+            return result;
+        
+        }//end method rollXSIdedDice           
+        public static Dictionary<string, int> RollCharStats()
+        {
+            int strengthRaw, agilityRaw, intelligenceRaw, luckRaw, charismaRaw,
+            constitutionRaw, maxHitPointsRaw, currentHitPointsRaw, maxRaw;
+
+            Dictionary<string, int> charStats = new Dictionary<string, int>();
+            // just keeps the while loop going
+            bool rolling = true;
+
+            while (rolling)
+            {
+                //roll stats for Fighter
+                #region actual rolls for character core stats
+                strengthRaw = Roller.rollXSidedDice(6, 2, 3);
+                agilityRaw = Roller.rollXSidedDice(6, 2, 3);
+                intelligenceRaw = Roller.rollXSidedDice(6, 2, 3);
+                luckRaw = Roller.rollXSidedDice(6, 2, 3);
+                charismaRaw = Roller.rollXSidedDice(6, 2, 3);
+                constitutionRaw = Roller.rollXSidedDice(6, 2, 3);
+                maxHitPointsRaw = (constitutionRaw * 4);
+                maxRaw = maxHitPointsRaw;
+                currentHitPointsRaw = maxRaw;
+                #endregion
+
+                #region displaying stats to the user and prompting to continue or roll again
+                Console.WriteLine("***Your Character Rolls***\n");
+                Console.WriteLine("Your strength is:   \t\t {0}  \n", strengthRaw);
+                Console.WriteLine("Your agility is:\t\t  {0} \n", agilityRaw);
+                Console.WriteLine("Your intelligence is:\t\t  {0} \n", intelligenceRaw);
+                Console.WriteLine("Your luck is:\t\t\t  {0} \n", luckRaw);
+                Console.WriteLine("Your charisma is:\t\t  {0} \n", charismaRaw);
+                Console.WriteLine("Your constitution is:\t\t  {0} \n", constitutionRaw);
+                Console.WriteLine("Your max hit points are:\t  {0} \n", maxHitPointsRaw);
+                Console.WriteLine("Your current hit points are:\t  {0} \n", currentHitPointsRaw);
+
+                Console.Write("Are you satisfied with your stats and ready to proceed? (Y/N)\n ");
+                string input = Console.ReadLine();
+                #endregion
+
+
+                // if statement that checks if the user wishes to continue or re-roll
+                // and adds stats rolled in last region to the Dictionary returned in
+                // this method
+                #region if statement
+
+                if (input == "Y" || input == "y")
+                {// adding rolled attributes to dictionary 
+                    charStats.Add("Strength", strengthRaw);
+                    charStats.Add("Agility", agilityRaw);
+                    charStats.Add("Intelligence", intelligenceRaw);
+                    charStats.Add("Luck", luckRaw);
+                    charStats.Add("Charisma", charismaRaw);
+                    charStats.Add("Constitution", constitutionRaw);
+                    charStats.Add("MaxHitPoints", maxHitPointsRaw);
+                    charStats.Add("CurrentHitPoints", currentHitPointsRaw);
+                    rolling = false;
+                    break;
+                }
+                else if (input == "N" || input == "n")
+                {// continue loop
+                    rolling = true;
+                    Console.Clear();
+                }
+                else
+                {// try again if fat-finger
+                    Console.WriteLine("Please enter a valid input");
+                    #endregion
+                }// end if
+
+            }// end while
+
+
+            return charStats;
+
+        }//end RollCharStats method 
+        #region Obsolete static rolling code (by dice type)
+        /*
+    #region 4-sided die rolling method group
+    //roll 1d4
+    public int rollFourSidedDie(int die4)
+    {
+        die4 = (int)(generator.NextDouble() * 4) + 1;
+        return die4;
+    }
+
+    //roll 2d4
+    public int roll2FourSidedDice(int die4, int secondDie4)
+    {
+        die4 = (int)(generator.NextDouble() * 4) + 1;
+        secondDie4 = (int)(generator.NextDouble() * 4) + 1;
+        return die4 + secondDie4;
+    }
+
+    //roll 3d4
+    public int roll3FourSidedDice(int die4, int secondDie4, int thirdDie4)
+    {
+        die4 = (int)(generator.NextDouble() * 4) + 1;
+        secondDie4 = (int)(generator.NextDouble() * 4) + 1;
+        thirdDie4 = (int)(generator.NextDouble() * 4) + 1;
+        return die4 + secondDie4 + thirdDie4;
+    }
+    #endregion
+
+    #region 6-sided die rolling method group
+    //roll 1d6
+    public int rollSixSidedDie(int die6)
+    {
+        die6 = (int)(generator.NextDouble() * 6) + 1;
+        return die6;
+    }
+
+    //roll 2d6
+    public int roll2SixSidedDice(int die6, int secondDie6)
+    {
+        die6 = (int)(generator.NextDouble() * 6) + 1;
+        secondDie6 = (int)(generator.NextDouble() * 6) + 1;
+        return die6 + secondDie6;
+    }
+
+    //roll 3d6
+    public int roll3SixSidedDice(int die6, int secondDie6, int thirdDie6)
+    {
+        die6 = (int)(generator.NextDouble() * 6) + 1;
+        secondDie6 = (int)(generator.NextDouble() * 6) + 1;
+        thirdDie6 = (int)(generator.NextDouble() * 6) + 1;
+        return die6 + secondDie6 + thirdDie6;
+    }
+    #endregion
+
+    #region 8-sided die rolling method group
+
+    //roll 1d8
+    public int rollEightSidedDie(int die8)
+    {
+        die8 = (int)(generator.NextDouble() * 8) + 1;
+        return die8;
+    }
+
+    //roll 2d8
+    public int roll2EightSidedDice(int die8, int secondDie8)
+    {
+        die8 = (int)(generator.NextDouble() * 8) + 1;
+        secondDie8 = (int)(generator.NextDouble() * 8) + 1;
+        return die8 + secondDie8;
+    }
+
+    //roll 3d8
+    public int roll3EightSidedDice(int die8, int secondDie8, int thirdDie8)
+    {
+        die8 = (int)(generator.NextDouble() * 8) + 1;
+        secondDie8 = (int)(generator.NextDouble() * 8) + 1;
+        thirdDie8 = (int)(generator.NextDouble() * 8) + 1;
+        return die8 + secondDie8 + thirdDie8;
+    }
+    #endregion
+
+    #region 10-sided die rolling method group
+    //roll 1d10
+    public int rollTenSidedDie(int die10)
+    {
+        die10 = (int)(generator.NextDouble() * 10) + 1;
+        return die10;
+    }
+
+    //roll 2d10
+    public int roll2TenSidedDice(int die10, int secondDie10)
+    {
+        die10 = (int)(generator.NextDouble() * 10) + 1;
+        secondDie10 = (int)(generator.NextDouble() * 10) + 1;
+        return die10 + secondDie10;
+    }
+
+    //roll 3d10
+    public int roll3TenSidedDice(int die10, int secondDie10, int thirdDie10)
+    {
+        die10 = (int)(generator.NextDouble() * 10) + 1;
+        secondDie10 = (int)(generator.NextDouble() * 10) + 1;
+        thirdDie10 = (int)(generator.NextDouble() * 10) + 1;
+        return die10 + secondDie10 + thirdDie10;
+    }
+    #endregion
+    */
+        #endregion
+
+
+    }//end class
+
+}//end namespace
+
+    
+
